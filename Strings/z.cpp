75c0aee21b24c3db8abfa0d150d9b914afd44856
pgoq <97684729+pgoq@users.noreply.github.com>
2025-07-06 13:59:19 -0300

    fix typo in z

--- Strings/z.cpp
+++ Strings/z.cpp
@@ -2,13 +2,13 @@
 
 // z[i] = tamanho do maior prefixo comecando de i que tambem é prefixo de s
 vector<int> ZFunc(const string &s) {
-  vector<int> z(v.size(), 0);
-  int n = (int) v.size(), a = 0, b = 0;
+  vector<int> z(s.size(), 0);
+  int n = (int) s.size(), a = 0, b = 0;
   if(!z.empty()) z[0] = 0;
   for(int i = 1; i < n; i++) {
     int end = i; 
     if (i < b) end = min(i + z[i - a], b);
-    while(end < n && v[end] == v[end - i]) ++end;
+    while(end < n && s[end] == s[end - i]) ++end;
     z[i] = end - i; if(end > b) a = i, b = end;
   }
   return z;
