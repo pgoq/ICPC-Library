12659216b5e4d2bfda7ec28d5a0e896233c63fc3
pgoq <97684729+pgoq@users.noreply.github.com>
2025-09-08 13:41:54 -0300

    indent z

--- Strings/z.cpp
+++ Strings/z.cpp
@@ -1,15 +1,15 @@
 // Z
 
 // z[i] = tamanho do maior prefixo comecando de i que tambem é prefixo de s
-vector<int> ZFunc(const string &s) {
+vector<int> ZFunc(const string& s) {
     vector<int> z(s.size(), 0);
-    int n = (int) s.size(), a = 0, b = 0;
-    if(!z.empty()) z[0] = 0;
-    for(int i = 1; i < n; i++) {
-        int end = i; 
+    int n = (int)s.size(), a = 0, b = 0;
+    if (!z.empty()) z[0] = 0;
+    for (int i = 1; i < n; i++) {
+        int end = i;
         if (i < b) end = min(i + z[i - a], b);
-        while(end < n && s[end] == s[end - i]) ++end;
-        z[i] = end - i; if(end > b) a = i, b = end;
+        while (end < n && s[end] == s[end - i]) ++end;
+        z[i] = end - i; if (end > b) a = i, b = end;
     }
     return z;
 }
