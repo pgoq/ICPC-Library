a2f838c8214348341ee9ef2675e3fd99c008b093
pgoq <97684729+pgoq@users.noreply.github.com>
2025-11-04 21:28:04 -0300

    change knuth

--- DP/knuth.cpp
+++ DP/knuth.cpp
@@ -4,20 +4,43 @@
 //              i < k < j
 // pode usar se opt[i][j-1] <= opt[i][j] <= opt[i+1][j]
 
-int n, opt[ms][ms];
-ll dp[ms][ms];
-void knuth() {
-    for (int i = n; i >= 0; i--) { // limites entre 0 e n
-        dp[i][i + 1] = 0; opt[i][i + 1] = i; // caso base
-        for (int j = i + 2; j <= n; j++) {
-            dp[i][j] = inf; // long long inf
-            for (int k = opt[i][j - 1]; k <= opt[i + 1][j]; k++) {
-                if (dp[i][j] > dp[i][k] + dp[k][j]) {
-                    dp[i][j] = dp[i][k] + dp[k][j];
+ll solve(int i, int j){
+    if(i >= j) return 0ll;
+    ll &ans = dp[i][j];
+    if(~ans) return ans;
+    solve(i, j-1);
+    solve(i+1, j);
+    int optL = (opt[i][j-1] == -1 ? 0 : opt[i][j-1]);
+    int optR = (opt[i+1][j] == -1 ? n-1 : opt[i+1][j]);
+    ans = INF;
+    for(int k=max(i, optL); k<=min(j-1, optR); k++){
+        ll now = solve(i, k) + solve(k+1, j) + cost(i, j);
+        if(now <= ans){
+            opt[i][j] = k;
+            ans = now;
+        }
+    }
+    return ans;
+}
+
+ll knuth(){
+    memset(opt, -1, sizeof opt);
+    for(int i=n-1; i>=0; i--){
+        dp[i][i] = 0; // caso base
+        opt[i][i] = i;
+        for(int j=i+1; j<n; j++){
+            int optL = (((j == 0) || opt[i][j-1] == -1) ? 0 : opt[i][j-1]);
+            int optR = (opt[i+1][j] == -1 ? n-1 : opt[i+1][j]);
+            ll cst = cost(i, j);
+            dp[i][j] = INF;
+            for(int k=max(i, optL); k<=min(j-1, optR); k++){
+                ll now = dp[i][k] + dp[k+1][j] + cst;
+                if(now <= dp[i][j]){
+                    dp[i][j] = now;
                     opt[i][j] = k;
                 }
             }
-            dp[i][j] += c(i, j); // custo associado ao intervalo [i, j]
         }
     }
+    return dp[0][n-1];
 }
